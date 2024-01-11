19ba762f8a0989083ba4671bbaacd373bad645eb
akmanning <amanning@broadinstitute.org>
2024-01-11 12:42:18 -0500

    setting up functions

--- R/cohorts_summary_demographics.R
+++ R/cohorts_summary_demographics.R
@@ -1,23 +1,23 @@
-cohorts_summary_demographics <- function(new.data.set)
-{
+cohorts_summary_demographics <- function(new.data.set) {
+  # This function creates a table of demographics for a given dataset
+  # new.data.set is the dataset to be summarized
 
-  lapply(c('table1','IRdisplay'),
-         function(pkg_name) { if(! pkg_name %in% installed.packages()) { install.packages(pkg_name)} } )
-
-
-  library(table1)
-  library(IRdisplay)
 
   my.render.cont <- function(x) {
-    with(stats.apply.rounding(stats.default(x), digits=2), c("",
-                                                             "Mean (SD)"=sprintf("%s (&plusmn; %s)", MEAN, SD)))
+    with(stats.apply.rounding(stats.default(x), digits = 2), c("",
+      "Mean (SD)" = sprintf("%s (&plusmn; %s)", MEAN, SD)
+    ))
   }
   my.render.cat <- function(x) {
-    c("", sapply(stats.default(x), function(y) with(y,
-                                                    sprintf("%d (%0.0000f %%)", FREQ, PCT))))
+    c("", sapply(stats.default(x), function(y) {
+      with(
+        y,
+        sprintf("%d (%0.0000f %%)", FREQ, PCT)
+      )
+    }))
   }
 
-  #options(repr.vector.quote=FALSE)
+  # options(repr.vector.quote=FALSE)
   label(new.data.set$age) <- "Age"
   units(new.data.set$age) <- "Years"
   label(new.data.set$gender) <- "Reported Gender"
@@ -26,9 +26,11 @@ cohorts_summary_demographics <- function(new.data.set)
   label(new.data.set$ethnicity) <- "Reported Ethnicity"
 
 
-  to.html <- table1(~ age + gender + sex_at_birth + race + ethnicity, data=new.data.set,
-                    render.continuous=my.render.cont,
-                    render.categorical=my.render.cat)
+  to.html <- table1::table1(~ age + gender + sex_at_birth + race + ethnicity,
+    data = new.data.set,
+    render.continuous = my.render.cont,
+    render.categorical = my.render.cat
+  )
 
-  display_html(to.html)
+  IRdisplay::display_html(to.html)
 }
--- R/create_drug_exposure_dataset_with_OMOP_CONCEPT_ID.R
+++ R/create_drug_exposure_dataset_with_OMOP_CONCEPT_ID.R
@@ -1,24 +1,40 @@
+#' create a dataset with drug exposure data for a given OMOP concept ID
+#' @param OMOPCONCEPTID the OMOP concept ID for the drug exposure
+#' @param query_sql the SQL query to use to create the dataset
+#' @return a list with the OMOP concept ID, the query SQL, and the path to the query results
+#' @export
+#' @examples
+#' OMOPCONCEPTID <- 21600713
+#' tmp <- create_drug_exposure_dataset_with_OMOP_CONCEPT_ID(OMOPCONCEPTID)
+#' tmp.person_ids <- get_person_ids_with_drug_exposure(tmp$query_result_path)
 create_drug_exposure_dataset_with_OMOP_CONCEPT_ID <- function(OMOPCONCEPTID, query_sql = drug_exposure_query_sql) {
-  query_sql <- query_sql %>% str_replace_all("XYZ_OMOPCONCEPTID_XYZ",as.character(OMOPCONCEPTID))
+  query_sql <- query_sql %>% str_replace_all("XYZ_OMOPCONCEPTID_XYZ", as.character(OMOPCONCEPTID))
 
   query_result_path <- file.path(
     Sys.getenv("WORKSPACE_BUCKET"),
     "bq_exports",
     "drug_exposure_OMOP_ID_XYZ_OMOPCONCEPTID_XYZ",
-    "drug_exposure_OMOP_ID_XYZ_OMOPCONCEPTID_XYZ_*.csv") %>% str_replace_all("XYZ_OMOPCONCEPTID_XYZ",as.character(OMOPCONCEPTID))
+    "drug_exposure_OMOP_ID_XYZ_OMOPCONCEPTID_XYZ_*.csv"
+  ) %>% str_replace_all("XYZ_OMOPCONCEPTID_XYZ", as.character(OMOPCONCEPTID))
 
-  message(str_glue('The data will be written to {query_result_path}. Use this path when reading ',
-                   'the data into your notebooks in the future.'))
+  message(str_glue(
+    "The data will be written to {query_result_path}. Use this path when reading ",
+    "the data into your notebooks in the future."
+  ))
 
-  if(system(paste0("gsutil ls ",query_result_path))) {
+  if (system(paste0("gsutil ls ", query_result_path))) {
     print("Data set has not been previously created. Creating now.")
     bq_table_save(
       bq_dataset_query(Sys.getenv("WORKSPACE_CDR"), query_sql,
-                       billing = Sys.getenv("GOOGLE_PROJECT")),
+        billing = Sys.getenv("GOOGLE_PROJECT")
+      ),
       query_result_path,
-      destination_format = "CSV")
-  } else print ("Data set has been previously created.")
+      destination_format = "CSV"
+    )
+  } else {
+    print("Data set has been previously created.")
+  }
 
 
-  return(list(OMOPCONCEPTID=OMOPCONCEPTID, query_sql=query_sql, query_result_path=query_result_path))
+  return(list(OMOPCONCEPTID = OMOPCONCEPTID, query_sql = query_sql, query_result_path = query_result_path))
 }
