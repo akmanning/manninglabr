88e1e9e8f4c2113297b460a1adfaf288d1041eb2
akmanning <amanning@broadinstitute.org>
2024-01-12 14:31:56 -0500

    added age variable creation

--- R/data.R
+++ R/data.R
@@ -4,6 +4,7 @@
 #' quote: This query represents dataset "all_insulin_example" for domain "drug" and was generated for All of Us Controlled Tier Dataset v7
 #' Documentation exists in the internally available notebook: Phenotype Harmonization in the All of Us Cohort v7 12Dec2023 AKM.ipynb
 #'
+#' @format ## `drug_exposure_query_sql` is a character vector with 1 element
 #' @source <https://workbench.researchallofus.org/workspaces/aou-rw-c8211e3d/commonmetabolicdiseasegeneticassociationanalysisv7/notebooks/preview/Phenotype%20Harmonization%20in%20the%20All%20of%20Us%20Cohort%20v7%2012Dec2023%20AKM.ipynb>
 #'
 "drug_exposure_query_sql"
