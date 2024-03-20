f508cc57f2c019e1b93a5ff0108c7cc7c30d6f19
akmanning <amanning@broadinstitute.org>
2024-03-20 13:18:16 -0400

    added filter for missing numeric values in value_as_number column

--- R/get_filtered_measurement_df.R
+++ R/get_filtered_measurement_df.R
@@ -1,6 +1,7 @@
 #' @title get_filtered_measurement_df
 #' @description This function takes an data frame created by the create_measurement_dataset_with_OMOP_CONCEPT_ID function and applies the filters operator_concept_name, unit_concept_name, visit_occurrence_concept_name and measurement_type_concept_name
 #' @param dataset_measurement_df Data frame as output from create_measurement_dataset_with_OMOP_CONCEPT_ID function
+#' @param filter_missing_values_as_number filter missing values_as_number rows. example TRUE or FALSE
 #' @param filter_min minimum value for filtering with >=
 #' @param filter_max maximum value for filtering with <=
 #' @param filter_unit_concept_name list of string values for filtering. example c("value1", "value2")
@@ -9,9 +10,12 @@
 #' @param filter_measurement_type_concept_name  list of string values for filtering. example c("value1", "value2")
 #' @return A data frame with the filtered values
 #' @export
-get_filtered_measurement_df <- function(dataset_measurement_df,filter_min=NULL, filter_max=NULL, filter_unit_concept_name=NULL, filter_operator_concept_name=NULL, filter_visit_occurrence_concept_name=NULL, filter_measurement_type_concept_name=NULL) {
+get_filtered_measurement_df <- function(dataset_measurement_df,filter_missing_values_as_number=FALSE,filter_min=NULL, filter_max=NULL, filter_unit_concept_name=NULL, filter_operator_concept_name=NULL, filter_visit_occurrence_concept_name=NULL, filter_measurement_type_concept_name=NULL) {
   dataset_measurement_df_tmp <- dataset_measurement_df
 
+  if(filter_missing_values_as_number==TRUE) {
+    dataset_measurement_df_tmp <- dataset_measurement_df_tmp %>% filter(!is.na(value_as_number))
+  }
   if(!is.null(filter_min)) {
     dataset_measurement_df_tmp <- dataset_measurement_df_tmp %>% filter(value_as_number >= filter_min)
   }
