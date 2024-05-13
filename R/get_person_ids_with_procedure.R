ef5ee4201cd47b8aa9725515b2ebb26cc09fd9fb
akmanning <amanning@broadinstitute.org>
2024-05-13 13:43:09 -0400

    updated procedure functions

---
+++ R/get_person_ids_with_procedure.R
@@ -0,0 +1,16 @@
+#' @title Get the IDs of the people in a procedure dataset
+#' @description
+#' This function returns the person_ids of the people in a procedure dataset.
+#' @param query_result_path The path to the query result.
+#' @return A vector of person_ids.
+#' @export
+get_person_ids_with_procedure <-  function(query_result_path) {
+  dataset_procedure_df <- read_procedure_bq_export_from_workspace_bucket(query_result_path)
+  if (nrow(dataset_condition_df) == 0) {
+    print("No data found. Please check your OMOP_CONCEPT_ID.")
+  } else {
+    print(paste("There are", nrow(dataset_procedure_df), "rows in the dataset."))
+    print(paste("There are", length(unique(dataset_procedure_df$person_id)), "unique study IDs."))
+  }
+  return(unique(dataset_procedure_df$person_id))
+}
