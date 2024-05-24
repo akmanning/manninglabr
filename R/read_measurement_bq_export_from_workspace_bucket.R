b8a9ab5a9b31b21c432fcec424c0baa1f2d1c8ae
akmanning <amanning@broadinstitute.org>
2024-05-24 17:08:46 -0400

    added package names to all functions

--- R/read_measurement_bq_export_from_workspace_bucket.R
+++ R/read_measurement_bq_export_from_workspace_bucket.R
@@ -5,27 +5,27 @@
 #' @return A tibble of drug exposure data.
 #' @export
 read_measurement_bq_export_from_workspace_bucket <- function(export_path) {
-  col_types <- readr::cols(standard_concept_name = col_character(),
-                    standard_concept_code = col_character(),
-                    standard_vocabulary = col_character(),
-                    measurement_type_concept_name = col_character(),
-                    operator_concept_name = col_character(),
-                    value_as_concept_name = col_character(),
-                    unit_concept_name = col_character(),
-                    visit_occurrence_concept_name = col_character(),
-                    measurement_source_value = col_character(),
-                    source_concept_name = col_character(),
-                    source_concept_code = col_character(),
-                    source_vocabulary = col_character(),
-                    unit_source_value = col_character(),
-                    value_source_value = col_character())
+  col_types <- readr::cols(standard_concept_name = readr::col_character(),
+                    standard_concept_code = readr::col_character(),
+                    standard_vocabulary = readr::col_character(),
+                    measurement_type_concept_name = readr::col_character(),
+                    operator_concept_name = readr::col_character(),
+                    value_as_concept_name = readr::col_character(),
+                    unit_concept_name = readr::col_character(),
+                    visit_occurrence_concept_name = readr::col_character(),
+                    measurement_source_value = readr::col_character(),
+                    source_concept_name = readr::col_character(),
+                    source_concept_code = readr::col_character(),
+                    source_vocabulary = readr::col_character(),
+                    unit_source_value = readr::col_character(),
+                    value_source_value = readr::col_character())
   bind_rows(
-    map(system2('gsutil', args = c('ls', export_path), stdout = TRUE, stderr = TRUE),
+    purrr::map(system2('gsutil', args = c('ls', export_path), stdout = TRUE, stderr = TRUE),
         function(csv) {
-          message(str_glue('Loading {csv}.'))
-          chunk <- read_csv(pipe(str_glue('gsutil cat {csv}')), col_types = col_types, show_col_types = FALSE)
+          message(stringr::str_glue('Loading {csv}.'))
+          chunk <- readr::read_csv(pipe(stringr::str_glue('gsutil cat {csv}')), col_types = readr::col_types, show_col_types = FALSE)
           if (is.null(col_types)) {
-            col_types <- spec(chunk)
+            col_types <- readr::spec(chunk)
           }
           chunk
         }))
