674ebfa1de82d58fd2a3ca7a0c6a76583c9127ae
akmanning <amanning@broadinstitute.org>
2024-05-24 17:38:51 -0400

    added filter by missing number default true

--- R/get_measurement_table_summary.R
+++ R/get_measurement_table_summary.R
@@ -2,13 +2,18 @@
 #' @description This function takes an OMOP Concept ID in the Measurement domain returns a summary of the non-missing values stratified by standard_concept_name, measurement_type_concept_name, operator_concept_name,unit_concept_name, and visit_occurrence_concept_name.
 #' @return A data frame with the following columns: standard_concept_name, measurement_type_concept_name, operator_concept_name, unit_concept_name, visit_occurrence_concept_name, and summary statistics.
 #' @param dataset_measurement_df Data frame as output from create_measurement_dataset_with_OMOP_CONCEPT_ID function
+#' @param filter_missing_values_as_number logical value for filtering missing values. Default is FALSE.
 #' @param filter_unit_concept_name list of string values for filtering. example c("value1", "value2")
 #' @param filter_operator_concept_name  list of string values for filtering. example c("value1", "value2")
 #' @param filter_visit_occurrence_concept_name  list of string values for filtering. example c("value1", "value2")
 #' @param filter_measurement_type_concept_name  list of string values for filtering. example c("value1", "value2")
 #' @export
-get_measurement_table_summary <- function(dataset_measurement_df,filter_unit_concept_name=NULL, filter_operator_concept_name=NULL, filter_visit_occurrence_concept_name=NULL, filter_measurement_type_concept_name=NULL) {
+get_measurement_table_summary <- function(dataset_measurement_df,filter_missing_values_as_number=FALSE,filter_unit_concept_name=NULL, filter_operator_concept_name=NULL, filter_visit_occurrence_concept_name=NULL, filter_measurement_type_concept_name=NULL) {
   dataset_measurement_df_tmp <- dataset_measurement_df
+
+  if(filter_missing_values_as_number) {
+
+    dataset_measurement_df_tmp <- dataset_measurement_df_tmp %>% dplyr::filter(!is.na(value_as_number))}
 
   if(length(filter_unit_concept_name)>0) {
   dataset_measurement_df_tmp <- dataset_measurement_df_tmp %>% dplyr::filter(unit_concept_name %in% filter_unit_concept_name)
