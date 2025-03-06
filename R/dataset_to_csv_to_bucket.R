caa930ace4775c7a4b1966da668b6ba22d44de00
Alisa Manning <amanning@broadinstitute.org>
2025-03-06 22:16:52 +0000

    made the bucket_path_to_save a function input

--- R/dataset_to_csv_to_bucket.R
+++ R/dataset_to_csv_to_bucket.R
@@ -3,7 +3,7 @@
 #' @param name_to_save the name of the dataset to be saved
 #' @return a csv file in the bucket
 #' @export
-dataset_to_csv_to_bucket <- function(name_to_save, dataset_df = all_participants$dataset_person_df) {
+dataset_to_csv_to_bucket <- function(name_to_save, dataset_df = all_participants$dataset_person_df, bucket_path_to_save="data") {
   # create time stamp and get username
   today <-  strftime(lubridate::now(), "%Y-%m-%d")
   owner <- str_replace(string = Sys.getenv("OWNER_EMAIL"),pattern="@researchallofus.org",replacement = "")
@@ -18,10 +18,10 @@ dataset_to_csv_to_bucket <- function(name_to_save, dataset_df = all_participants
 
   # Upload file to google bucket
   # Copy the file from current workspace to the bucket
-  system(paste0("gsutil cp ./", filename, " ", my_bucket, "/data/"), intern=T)
+  system(paste0("gsutil cp ./", filename, " ", my_bucket, "/",bucket_path_to_save,"/"), intern=T)
 
   # Check if file is in the bucket
   print(paste("Listing files in bucket with the label:", name_to_save, "user and the filename:",filename))
-  print(system(paste0("gsutil ls ", my_bucket, "/data/*.csv | grep ",name_to_save), intern=T))
+  print(system(paste0("gsutil ls ", my_bucket, "/",bucket_path_to_save,"/*.csv | grep ",name_to_save), intern=T))
 
   }
--- R/html_to_bucket.R
+++ R/html_to_bucket.R
@@ -3,7 +3,7 @@
 #' @param name_to_save the name of the html file to be saved
 #' @return a html file in the bucket
 #' @export
-html_to_bucket <- function(name_to_save) {
+html_to_bucket <- function(name_to_save, bucket_path_to_save="data") {
   # create time stamp and get username
   today <-  strftime(lubridate::now(), "%Y-%m-%d")
   owner <- stringr::str_replace(string = Sys.getenv("OWNER_EMAIL"),pattern="@researchallofus.org",replacement = "")
@@ -16,10 +16,10 @@ html_to_bucket <- function(name_to_save) {
 
   # Upload file to google bucket
   # Copy the file from current workspace to the bucket
-  system(paste0("gsutil cp ./", filename, " ", my_bucket, "/data/"), intern=T)
+  system(paste0("gsutil cp ./", filename, " ", my_bucket, "/",bucket_path_to_save,"/"), intern=T)
 
   # Check if file is in the bucket
   print(paste("Listing files in bucket with the label:", name_to_save, "user and the filename:",filename))
-  print(system(paste0("gsutil ls ", my_bucket, "/data/*.csv | grep ",name_to_save), intern=T))
+  print(system(paste0("gsutil ls ", my_bucket, "/",bucket_path_to_save,"/*.html | grep ",name_to_save), intern=T))
 
 }
